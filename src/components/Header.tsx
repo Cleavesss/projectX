ecb035ba7555f6a20f43fdb3aad0c3b08f986303
Смородинов Илья Константинович <cleavessss21dgtv@mail.ru>
2024-03-08 15:33:41 +0400

    Стартовый коммит ветки dev

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+
+
+    return (
+        <div className="header">
+            Тут будет навигационная панель
+        </div>    
+        
+    )
+}
+
+export default Header
