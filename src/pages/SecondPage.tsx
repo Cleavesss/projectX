726379f7fe785166d3b48cef3c998df7802aa016
Смородинов Илья Константинович <cleavessss21dgtv@mail.ru>
2024-03-08 20:19:14 +0400

    testCommitNew3branch

---
+++ src/pages/SecondPage.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function SecondPage(){
+    return (
+        <div className='main'>
+            <div className='menu'>
+                Тут будет меню
+            </div>
+            <div>
+                <div className='content'>
+                    HelloWorld
+                    <div className='childContent'>
+                        Дочерний элемент
+                    </div>
+                </div>
+            </div>
+        </div>    
+    )
+}
